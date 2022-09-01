fb066e138e1b419b4f679863d090e31b07da7489
Jeremiah Lu <jeremiahlu09@gmail.com>
2022-09-01 03:16:57 +0000

    Update 4 files

--- src/components/Movies/index.js
+++ src/components/Movies/index.js
@@ -1,7 +1,28 @@
-function Movies() {
+import { NavLink, Route, Switch, useParams} from 'react-router-dom';
+import MovieDetails from '../MovieDetails';
+// import { useParams } from 'react';
+
+function Movies({movies}) {
   return (
     <div className='comp orange'>
       <h1>Movies Component</h1>
+      <nav> {
+          movies.map((movieId) => {
+              return(
+                  <NavLink to={`/movies/${movieId.id}`}>
+                     <h1>
+                         MovieDetails Component
+                     </h1>
+                  </NavLink>
+              )
+          })
+        }
+      </nav>
+      <Switch>
+      <Route path='/movies/:movieId'>
+          <MovieDetails movies={movies}/>
+      </Route>
+      </Switch>
     </div>
   );
 }
